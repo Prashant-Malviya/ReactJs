a30d5b872dbd0f23ff288f71e03e1bef02516386
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-10-29 15:36:48 +0530

    understand how props works in react

---
+++ tut4TailwindAndProps/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    userName : 'prashant',
+    age : 25
+  }
+  let newArr = [1,2,3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-5 rounded-2xl mb-5'>Tailwind Test</h1>
+     <Card name ='prashant' someObj = {myObj} arr = {newArr}/>
+     <Card />
+    </>
+  )
+}
+
+export default App
