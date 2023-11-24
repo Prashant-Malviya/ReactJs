2387a4d8833b346638ab681b65719273349d2acb
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-11-24 09:47:57 +0530

    Again built password generator  Project in React

--- tut5BgChanger/src/App.jsx
+++ tut5BgChanger/src/App.jsx
@@ -4,13 +4,13 @@ function App() {
   const [color, setColor] = useState("blue")
 
   return (
-    
+
       <div className="w-full h-screen duration-200"  style={{backgroundColor: color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
       <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl'>
       <button onClick={()=> setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"red"}}>Red
       </button>
-      <button onClick={()=> setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:"Yellow"}}>Yellow
+      <button onClick={()=> setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style= {{backgroundColor:"Yellow"}}>Yellow
       </button>
       <button onClick={()=> setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"Green"}}>Green
       </button>
