e008ec371765285a5023bf3585a23194b4bcb666
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-11-25 17:22:08 +0530

    Develop Currency Converter App using React

--- tut7CustomeHooksInReact/src/App.jsx
+++ tut7CustomeHooksInReact/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [amount,setAmount] = useState(0)
   const [from,setFrom] = useState("usd")
   const [to,setTo] = useState("inr")
-  const [convertedAmount,setConvertedAmout] = useState(0);
+  const [convertedAmount,setConvertedAmount] = useState(0);
 
   const currencyInfo = useCurrencyInfo(from);
   
@@ -17,12 +17,12 @@ function App() {
   const swap = () =>{
     setFrom(to);
     setTo(from);
-    setConvertedAmout(amount);
+    setConvertedAmount(amount);
     setAmount(convertedAmount);
   }
 
   const convert = ()=>{
-    setConvertedAmout(amount*currencyInfo(to))
+    setConvertedAmount(amount*currencyInfo(to))
   }
 
   const BackgroundImage = "https://img.freepik.com/premium-vector/forex-trading-wallpaper_23-2148584743.jpg"
