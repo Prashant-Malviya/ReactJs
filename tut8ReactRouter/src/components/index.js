e5af10989eeadbabd64b96568adad476e9c931c8
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-11-26 18:23:39 +0530

    React Router Done

---
+++ tut8ReactRouter/src/components/index.js
@@ -0,0 +1,5 @@
+// import Home from './components/Home'
+// import About from './components/About'
+// import Contact from './components/Contact'
+
+// export {Home,About,Contact}
--- tut8ReactRouter/src/components/index.jsx
+++
@@ -1,3 +0,0 @@
-import Home from './components/Home/Home.jsx'
-import About from './components/About/About.jsx'
-import Contact from './components/Contact/Contact.jsx'
