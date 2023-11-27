75d8f366b53ebf2546358a6dae17b4d08c1c5e72
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-11-27 23:57:12 +0530

    React Router With Web App Completed

---
+++ A Web App Using React Router/src/Components/GitHub/GitHub.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function GitHub() {
+
+    const data = useLoaderData();
+
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/prashant-malviya")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   });
+
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      GitHub Bio : {data.bio}
+      <img src={data.avatar_url} alt="" />
+    </div>
+  );
+}
+
+export default GitHub;
+
+
+export const githubInfoLoader = async ()=>{
+    const response = await fetch('https://api.github.com/users/prashant-malviya')
+    return response.json() 
+  }
